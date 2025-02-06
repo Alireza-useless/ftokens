bf43e9e1bca8a1c5af726d2314001d701499c6cd
Alireza-useless <aalirezi42@gmail.com>
2025-02-06 06:40:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from concurrent.futures import ThreadPoolExecutor
 
 # مشخصات گیلد و چنل ثابت
 guild_id = "1182277701877366834"  # گیلد
-chid = "1182279162241749042"      # چنل
+chid = "1182279162241749042"  # چنل
 
 # خواندن توکن‌ها از فایل
 tokenlist = open("tokens.txt").read().splitlines()
 executor = ThreadPoolExecutor(max_workers=10)  # تعداد کارگرها را کمتر کنید
+
 
 def run(token):
     try:
@@ -20,8 +21,26 @@ def run(token):
         heartbeat_interval = hello['d']['heartbeat_interval']
 
         # ارسال پیام‌های اولیه برای اتصال به سرور
-        ws.send(dumps({"op": 2, "d": {"token": token, "properties": {"$os": "windows", "$browser": "Discord", "$device": "desktop"}}}))
-        ws.send(dumps({"op": 4, "d": {"guild_id": guild_id, "channel_id": chid}}))  # فقط اتصال به چنل و گیلد
+        ws.send(
+            dumps({
+                "op": 2,
+                "d": {
+                    "token": token,
+                    "properties": {
+                        "$os": "windows",
+                        "$browser": "Discord",
+                        "$device": "desktop"
+                    }
+                }
+            }))
+        ws.send(
+            dumps({
+                "op": 4,
+                "d": {
+                    "guild_id": guild_id,
+                    "channel_id": chid
+                }
+            }))  # فقط اتصال به چنل و گیلد
 
         while True:
             sleep(heartbeat_interval / 1000)
@@ -31,6 +50,7 @@ def run(token):
                 break  # در صورت بروز خطا، اتصال قطع می‌شود و باید دوباره وصل شود
     except Exception as e:
         print(f"Error: {e}")
+
 
 # اجرا برای تمامی توکن‌ها از فایل tokens.txt
 i = 0
